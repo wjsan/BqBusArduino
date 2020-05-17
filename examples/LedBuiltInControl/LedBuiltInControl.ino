e5a21196f4a238b8b4e26f9071dcab919190f1d7
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2020-05-17 12:15:59 -0300

    Added keywords and changed vstudio name to pcApp

--- examples/LedBuiltInControl/LedBuiltInControl.ino
+++ examples/LedBuiltInControl/LedBuiltInControl.ino
@@ -12,17 +12,17 @@
 
 #include "BqBus.h"
 
-BqBus vstudio(1);     //Instance to share 1 register with vstudio application
+BqBus pcApp(1);      //Instance to share 1 register with pcApp application
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);                               //Initialize serial port
-  vstudio.setBus(Serial);                           //Set serial port used to communicate with vstudio application
+  pcApp.setBus(Serial);                             //Set serial port used to communicate with pcApp application
   pinMode(LED_BUILTIN, OUTPUT);                     //Configure board default output to control on-board led
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  vstudio.communicationTask();                      //Performs communication betteen arduino and vstudio application
-  digitalWrite(LED_BUILTIN, vstudio.getReg(0));     //Read register with address zero and write his values on led
+  pcApp.communicationTask();                      //Performs communication betteen arduino and pcApp application
+  digitalWrite(LED_BUILTIN, pcApp.getReg(0));     //Read register with address zero and write his values on led
 }
